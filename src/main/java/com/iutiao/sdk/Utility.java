3fc1f5c8501c1757394bf570d23e028101955ba5
yxy <yxy.870@gmail.com>
2015-11-04 11:21:37 +0800

    add function to generate random strings
    Signed-off-by: yxy <yxy.870@gmail.com>

--- src/main/java/com/iutiao/sdk/Utility.java
+++ src/main/java/com/iutiao/sdk/Utility.java
@@ -1,12 +1,22 @@
 package com.iutiao.sdk;
 
+import java.util.Random;
+
 /**
  * Created by yxy on 15/11/4.
  */
 public final class Utility {
 
+    static String alphanumeric = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
     public static String generateRandomString(int length) {
+        Random rand = new Random();
         StringBuffer buffer = new StringBuffer();
+        for (int i=0; i<length; i++) {
+            int idx = Math.abs((rand.nextInt() % alphanumeric.length()));
+            buffer.append(alphanumeric.charAt(idx));
+        }
         return buffer.toString();
     }
+
 }
