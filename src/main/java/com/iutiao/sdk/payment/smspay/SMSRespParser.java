8bb01a2cb7afa7dd0e6a9f5925a2125348e29c17
Ben <wenbin.private@gmail.com>
2016-05-24 15:07:53 +0800

    format of sms response change

--- src/main/java/com/iutiao/sdk/payment/smspay/SMSRespParser.java
+++ src/main/java/com/iutiao/sdk/payment/smspay/SMSRespParser.java
@@ -9,78 +9,40 @@
 
 package com.iutiao.sdk.payment.smspay;
 
+import android.text.TextUtils;
+
 import com.iutiao.sdk.payment.PaymentResponseWrapper;
 
-import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserException;
-import org.xmlpull.v1.XmlPullParserFactory;
-
-import java.io.IOException;
-import java.io.StringReader;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Successful SMS response:
- * <Response>
- * <SmsText>answer message</SmsText>
- * </Response>
- * <p/>
- * Error SMS response:
- * <Response>
- * <ErrorText>error message</ErrorText> <SmsText>answer message</SmsText>
- * </Response>
- * <p/>
- * This class can parse the xml response to PaymentResponseWrapper
- * Created by Ben on 16/5/23.
+ * SMS response:ZTE.skuId.resultCode
  */
 public class SMSRespParser {
     public static PaymentResponseWrapper parse(String strXmlResp) {
         PaymentResponseWrapper paymentResponseWrapper;
         Map<String, Object> data = new HashMap<>();
-        String errorText = null;
-        String smsText = null;
-        XmlPullParserFactory factory = null;
-        try {
-            factory = XmlPullParserFactory.newInstance();
-            factory.setNamespaceAware(true);
-            XmlPullParser xpp = factory.newPullParser();
+        String[] strXmlResps = strXmlResp.split("\\.", 3);
+        if (strXmlResps.length != 3) {
+            paymentResponseWrapper = new PaymentResponseWrapper(data, new Exception("Format error:" + strXmlResp));
+            return paymentResponseWrapper;
+        }
 
-            xpp.setInput(new StringReader(strXmlResp));
-            int eventType = xpp.getEventType();
-            while (eventType != XmlPullParser.END_DOCUMENT) {
-                if (eventType == XmlPullParser.START_DOCUMENT) {
-                } else if (eventType == XmlPullParser.START_TAG) {
-                    if (xpp.getName().equals("ErrorText")) {
-                        errorText = xpp.nextText();
-                    }
-                    if (xpp.getName().equals("SmsText")) {
-                        smsText = xpp.nextText();
-                    }
-                } else if (eventType == XmlPullParser.END_TAG) {
-                } else if (eventType == XmlPullParser.TEXT) {
-                }
-                eventType = xpp.next();
-            }
-        } catch (XmlPullParserException e) {
-            e.printStackTrace();
-            paymentResponseWrapper = new PaymentResponseWrapper(data, new Exception("Server error，response is not xml format"));
-            return paymentResponseWrapper;
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        if (errorText == null && smsText == null) {
-            paymentResponseWrapper = new PaymentResponseWrapper(data, new Exception("Server error，" + strXmlResp));
+        String skuId = strXmlResps[1];
+        String resultCode = strXmlResps[2];
+        if(TextUtils.isEmpty(skuId)||TextUtils.isEmpty(resultCode)){
+            paymentResponseWrapper = new PaymentResponseWrapper(data, new Exception("Format error:" + strXmlResp));
             return paymentResponseWrapper;
         }
-        if (errorText == null) {
-            data.put(SMSPayment.RESP_SMS_SKU_ID, smsText);
+
+        if(resultCode.equals("0")){
+            data.put(SMSPayment.RESP_SMS_SKU_ID, skuId);
             paymentResponseWrapper = new PaymentResponseWrapper(data, null);
             return paymentResponseWrapper;
-        } else {
-            paymentResponseWrapper = new PaymentResponseWrapper(data, new Exception(errorText));
+        }else {
+            paymentResponseWrapper = new PaymentResponseWrapper(data, new Exception("Payment error:"+resultCode));
             return paymentResponseWrapper;
         }
-
     }
 }
